a4cc6a636427a19c37c604d9e68fee545139de4d
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2024-10-18 23:11:20 +0530

    Create Vowel Present

---
+++ String/VowelPresent.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class VowelPresent {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		String res = "";
+		int bool = 0;
+		res = str.toLowerCase();
+		for(int i=0; i<str.length(); i++) {
+			if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+				bool = 1;
+			}
+		}
+		if(bool == 1) {
+			System.out.println("yes");
+		}
+		else {
+			System.out.println("No");
+		}
+	}
+}
