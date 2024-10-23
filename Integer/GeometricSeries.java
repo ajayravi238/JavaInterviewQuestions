e072706fc2a0c2a84c7f9f4e46f0f75b759f1b18
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2024-10-23 22:05:51 +0530

    Create Geometric Series

---
+++ Integer/GeometricSeries.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class GeometricSeries {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int noOfElement = sc.nextInt();
+		int startTerm = sc.nextInt();
+		int diff = sc.nextInt();
+		int res = 0;
+		for(int i=0; i<noOfElement; i++) {
+			res = startTerm;
+			System.out.println(res);
+			res *= diff;
+			startTerm = res;
+		}
+	}
+
+}
