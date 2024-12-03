c7546103c28152451f8ea5c2487ee4ff62d50337
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2024-12-03 11:35:00 +0530

    Create Subtraction Of Array

---
+++ 2DArray/SubOfArray.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SubOfArray {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int[][] arr1 = new int[num][num];
+		for(int i=0; i<num; i++) {
+			for(int j=0; j<num; j++) {
+				arr1[i][j] = sc.nextInt();
+			}
+		}
+		int[][] arr2 = new int[num][num];
+		for(int i=0; i<num; i++) {
+			for(int j=0; j<num; j++) {
+				arr2[i][j] = sc.nextInt();
+			}
+		}	
+		int[][] sum = new int[num][num];
+		for(int i=0; i<num; i++) {
+			for(int j=0; j<num; j++) {
+				sum[i][j] = arr1[i][j] - arr2[i][j];
+			}
+		}
+		for(int i=0; i<num; i++) {
+			for(int j=0; j<num; j++) {
+				System.out.print(sum[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
